68118d85e9b244fff4f385e1f584b7cc06040b4b
Aibekin <aibekin_zh_007@mail.ru>
2024-11-02 19:42:00 +0500

    detecting hidden marker

--- main.py
+++ main.py
@@ -1,94 +1,3 @@
-# import cv2
-# import numpy as np
-# import math
-
-# def estimatePoseSingleMarkers(corners, marker_size, mtx, distortion):
-#     marker_points = np.array([[-marker_size / 2, marker_size / 2, 0],
-#                               [marker_size / 2, marker_size / 2, 0],
-#                               [marker_size / 2, -marker_size / 2, 0],
-#                               [-marker_size / 2, -marker_size / 2, 0]], dtype=np.float32)
-#     trash = []
-#     rvecs = []
-#     tvecs = []
-#     for c in corners:
-#         nada, R, t = cv2.solvePnP(marker_points, c, mtx, distortion, False, cv2.SOLVEPNP_IPPE_SQUARE)
-#         rvecs.append(R)
-#         tvecs.append(t)
-#         trash.append(nada)
-#     return rvecs, tvecs
-
-# def give_points(corners, index_id_1, index_id_2, index_id_3, index_id_4):
-#     ccx_1 = corners[index_id_1][0][1]
-#     ccx_2 = corners[index_id_2][0][0]
-#     ccx_3 = corners[index_id_3][0][2]
-#     ccx_4 = corners[index_id_4][0][3]
-#     point = np.array([
-#         [[ccx_2[0], ccx_2[1]],
-#         [ccx_1[0], ccx_1[1]],
-#         [ccx_3[0], ccx_3[1]], 
-#         [ccx_4[0], ccx_4[1]]]
-#     ], dtype=np.float32)
-#     return point
-
-# def find_center(corners):
-#     [x1, y1] = corners[0][0][0]
-#     [x2, y2] = corners[0][0][1]
-#     [x3, y3] = corners[0][0][2]
-#     [x4, y4] = corners[0][0][3]
-    
-#     x_center = (x1 + x2 + x3 + x4) / 4
-#     y_center = (y1 + y2 + y3 + y4) / 4
-#     return (x_center, y_center)
-
-# cap = cv2.VideoCapture(0)
-# aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
-# parameters = cv2.aruco.DetectorParameters()
-# camera_matrix = np.array([[4577.7689562929327, 0, 341.35776797777669],
-#                           [0, 1845.0965778655157, 189.94836015559704],
-#                           [0, 0, 1]], dtype=np.float32)
-# dist_coeffs = np.array([27.505983603416613, -651.00215745718265, -1.1253795213096234,
-#                         -0.18015690540560628, 4713.9105972395546])
-
-# while True:
-#     ret, frame = cap.read()
-    
-#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#     detector = cv2.aruco.ArucoDetector(aruco_dict, parameters)
-#     corners, ids, rejected = detector.detectMarkers(gray)
-#     cv2.aruco.drawDetectedMarkers(frame, corners, ids)
-    
-#     if ids is not None:
-#         rvecs, tvecs = estimatePoseSingleMarkers(corners, 0.1, camera_matrix, dist_coeffs)
-        
-#         indices_id_1 = np.where(ids == 1)[0]
-#         indices_id_2 = np.where(ids == 0)[0]
-#         indices_id_3 = np.where(ids == 5)[0]
-#         indices_id_4 = np.where(ids == 4)[0]
-    
-#         if indices_id_1.size > 0 and indices_id_4.size > 0 and indices_id_3.size > 0 and indices_id_2.size > 0:
-
-#             index_id_1 = indices_id_1[0]
-#             index_id_2 = indices_id_2[0]
-#             index_id_3 = indices_id_3[0]
-#             index_id_4 = indices_id_4[0]
-#             point = give_points(corners, index_id_1, index_id_2, index_id_3, index_id_4)
-
-#             lenBott = math.sqrt((point[0][2][0] - point[0][3][0])**2 + (point[0][2][1] - point[0][3][1])**2) / 2
-
-#             rvecs1, tvecs1 = estimatePoseSingleMarkers(point, 0.2, camera_matrix, dist_coeffs)
-#             cv2.drawFrameAxes(frame, camera_matrix, dist_coeffs, rvecs1[0], tvecs1[0], 0.01)
-#             cv2.drawContours(frame, [point.astype(np.int32)], -1, (160, 120, 255), thickness=2)
-#             x, y = find_center(corners)
-#             print(x, y)
-
-#     cv2.imshow("Frame", frame)
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-
-# cap.release()
-# cv2.destroyAllWindows()
-
-
 
 import cv2
 import numpy as np
@@ -224,25 +133,6 @@ def my_estimatePoseSingleMarkers(corners, marker_size, mtx, distortion):
 
 #     return rvecs, tvecs
 
-def rvec_to_mtx(rvec):
-    R, _ = cv2.Rodrigues(rvec)
-    return R
-
-def mtx_to_quat(R1):
-    quat = R.from_matrix(R1).as_quat()
-    return quat
-
-def find_avg_quat(rvec):
-    R1 = rvec_to_mtx(rvec[0])
-    R2 = rvec_to_mtx(rvec[1])
-    
-    quat1 = mtx_to_quat(R1)
-    quat2 = mtx_to_quat(R2)
-
-    avg_quat = (quat1 + quat2) / np.linalg.norm(quat1 + quat2)  # Нормализуем средний кватернион
-    average_rotation_matrix = R.from_quat(avg_quat).as_matrix()
-    return average_rotation_matrix
-
 def draw_axes_at_center_of_markers(frame, center_1, camera_matrix, dist_coeffs):
     if len(corners) < 3:
         print("Недостаточно маркеров для расчета центра.")
@@ -259,9 +149,6 @@ def draw_axes_at_center_of_markers(frame, center_1, camera_matrix, dist_coeffs):
     # Проецируем 3D оси на 2D изображение
     image_points, _ = cv2.projectPoints(axes, np.zeros((3, 1), dtype=np.float32), center, camera_matrix, dist_coeffs)
 
-    if len(image_points) < 4:
-        return
-
     # Рисуем оси
     origin = tuple(image_points[0].flatten().astype(int))  # Начальная точка (центр)
     x_axis = tuple(image_points[1].flatten().astype(int))  # Ось X
@@ -271,7 +158,7 @@ def draw_axes_at_center_of_markers(frame, center_1, camera_matrix, dist_coeffs):
     # Рисуем оси на изображении
     cv2.line(frame, origin, x_axis[:2], (255, 0, 0), 2)  # Ось X - красная
     cv2.line(frame, origin, y_axis[:2], (0, 255, 0), 2)  # Ось Y - зеленая
-    cv2.line(frame, origin, z_axis[:2], (0, 0, 255), 2)  # Ось Z - синяя
+    cv2.line(frame, origin, z_axis[:2], (0, 0, 255), 2)  # Ось Z - синяя (если н
 
 
 aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)  # Подставь нужный словарь
@@ -290,10 +177,6 @@ virtual_size = 0.02
 data = []
 data_y = []
 i = 0
-rvec = np.zeros((3, 1), dtype=np.float32)
-tvec = np.zeros((3, 1), dtype=np.float32)
-rvecs1 =[]
-tvecs1= []
 while True:
     ret, frame = cap.read()
     if not ret:
@@ -321,15 +204,6 @@ while True:
             # cv2.circle(frame, (int(x), int(y)), 5, (255,0,0), -1)
             lenBott = math.sqrt((point[0][2][0] - point[0][3][0])**2 + (point[0][2][1] - point[0][3][1])**2) / 2
             rvecs1, tvecs1, _ = my_estimatePoseSingleMarkers(point, virtual_size, camera_matrix, dist_coeffs)
-            R1 = cv2.Rodrigues(rvecs[0])
-            R2 = cv2.Rodrigues(rvecs[1])
-            
-            quat1 = R.from_matrix(R1).as_quat()
-            quat2 = R.from_matrix(R2).as_quat()
-            
-            avg_quat = (quat1 + quat2) / np.linalg.norm(quat1 + quat2)
-            average_rotation_matrix = R.from_quat(avg_quat).as_matrix()
-            print(average_rotation_matrix)
             cv2.drawFrameAxes(frame, camera_matrix, dist_coeffs, rvecs1[0], tvecs1[0], 0.01)
             cv2.drawContours(frame, [point.astype(np.int32)], -1, (160, 120, 255), thickness=2)
             
@@ -360,16 +234,29 @@ while True:
         
             ccx_4 = corners[index_id_4][0][3].astype(int)
 
-            point = np.array([
-                [[ccx_2[0], ccx_2[1]],   
+            p1 = np.array([ccx_2[0], ccx_2[1]])  # Маркер 1
+            p2 = np.array([ccx_3[0], ccx_3[1]])  # Маркер 2
+            p3 = np.array([ccx_4[0], ccx_4[1]])  # Маркер 3
+
+            # Середина между p1 и p2
+            midpoint = (p1 + p2) / 2
+
+            # Предсказанная точка как зеркальное отражение p3 относительно midpoint
+            predicted_point = 2 * midpoint - p3
+            points = np.array([
+                [[ccx_2[0], ccx_2[1]],
+                [predicted_point[0].astype(int), predicted_point[1].astype(int)],   
                 [ccx_3[0], ccx_3[1]], 
                 [ccx_4[0], ccx_4[1]]]
             ], dtype=np.float32)
-            print(tvecs1)
-            # rvec, tvec = my_estimatePoseSingleMarkers_3P(point, [424.75, 362.0], 0.04, camera_matrix, dist_coeffs)
-            draw_axes_at_center_of_markers(frame, [424.75, 362.0], camera_matrix, dist_coeffs)
+            print([predicted_point[0], predicted_point[1]])
+            rvec, tvec, _ = my_estimatePoseSingleMarkers(points, virtual_size, camera_matrix, dist_coeffs)
             cv2.drawFrameAxes(frame, camera_matrix, dist_coeffs, rvec[0], tvec[0], 0.01)
-            # cv2.drawContours(frame, [point.astype(np.int32)], -1, (160, 120, 255), thickness=2)
+
+            # rvec, tvec = my_estimatePoseSingleMarkers_3P(point, [424.75, 362.0], 0.04, camera_matrix, dist_coeffs)
+            # draw_axes_at_center_of_markers(frame, [424.75, 362.0], camera_matrix, dist_coeffs)
+            # cv2.drawFrameAxes(frame, camera_matrix, dist_coeffs, rvec[0], tvec[0], 0.01)
+            cv2.drawContours(frame, [points.astype(np.int32)], -1, (160, 120, 255), thickness=2)
 
     cv2.imshow('ArUco Marker Detection', frame)
 
