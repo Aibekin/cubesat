1014a7be9c412136a08a3f354890c86f361bdc70
Aibekin <77619101+Aibekin@users.noreply.github.com>
2025-01-29 11:27:45 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,267 +1,178 @@
-
 import cv2
 import numpy as np
-from scipy.spatial.transform import Rotation as R
-import math
-import random
-import funcs
+from funcs import calc_ang
+import time 
+import matplotlib.pyplot as plt
+from collections import deque
 
+marker_length = 0.03 
+board_size = 0.10 
+offset = board_size / 2  # Смещение для определения центра
+buffer_size =1000000
+roll_data = deque(maxlen=buffer_size)
+pitch_data = deque(maxlen=buffer_size)
+yaw_data = deque(maxlen=buffer_size)
+time_data = deque(maxlen=buffer_size)
 
-def calc_ang(tvecs1, rvecs1):
-    rvec_matrix, _ = cv2.Rodrigues(rvecs1[0])
-    proj_matrix = np.hstack((rvec_matrix, tvecs1[0]))
-    euler_angle = cv2.decomposeProjectionMatrix(proj_matrix)[6] # [deg]
-    
-    
-    
-    if len(tvecs) > 0:
-        # print("x : " + str(tvecs1[0][0]))
-        # print("y : " + str(tvecs1[0][1]))
-        # print("z : " + str(tvecs1[0][2]))
-        # print("roll : " + str(euler_angle[0]))
-        # print("pitch: " + str(euler_angle[1]))
-        # print("yaw  : " + str(euler_angle[2]))
-        return euler_angle[0], euler_angle[1]
-
-def calc_angle_rotation(tvecs1, rvecs1):
-    transform_translation_x = tvecs1[0][0]
-    transform_translation_y = tvecs1[0][1]
-    transform_translation_z = tvecs1[0][2]
-
-    rotation_matrix = np.eye(4)
-    rotation_matrix[0:3, 0:3] = cv2.Rodrigues(np.array(rvecs1[0]))[0]
-    r = R.from_matrix(rotation_matrix[0:3, 0:3])
-    quat = r.as_quat()   
-        
-    transform_rotation_x = quat[0] 
-    transform_rotation_y = quat[1] 
-    transform_rotation_z = quat[2] 
-    transform_rotation_w = quat[3] 
-    
-    roll_x, pitch_y, yaw_z = euler_from_quaternion(transform_rotation_x, 
-                                                transform_rotation_y, 
-                                                transform_rotation_z, 
-                                                transform_rotation_w)
-    
-    roll_x = math.degrees(roll_x)
-    pitch_y = math.degrees(pitch_y)
-    yaw_z = math.degrees(yaw_z)
-    # roll_x = (roll_x + 360) % 360
-    # pitch_y = (pitch_y + 180) % 180
-    # yaw_z = (yaw_z + 90) % 90
-    # print("transform_translation_x: {}".format(transform_translation_x))
-    # print("transform_translation_y: {}".format(transform_translation_y))
-    # print("transform_translation_z: {}".format(transform_translation_z))
-    print(str(roll_x))
-    # print("pitch_y: {}".format(pitch_y))
-    # print("yaw_z: {}".format(yaw_z))
-
-def euler_from_quaternion(x, y, z, w):
-  t0 = +2.0 * (w * x + y * z)
-  t1 = +1.0 - 2.0 * (x * x + y * y)
-  roll_x = math.atan2(t0, t1)
-      
-  t2 = +2.0 * (w * y - z * x)
-  t2 = +1.0 if t2 > +1.0 else t2
-  t2 = -1.0 if t2 < -1.0 else t2
-  pitch_y = math.asin(t2)
-      
-  t3 = +2.0 * (w * z + x * y)
-  t4 = +1.0 - 2.0 * (y * y + z * z)
-  yaw_z = math.atan2(t3, t4)
-      
-  return roll_x, pitch_y, yaw_z 
-
-
-def give_points(corners, index_id_1, index_id_2, index_id_3, index_id_4):
-
-    
-    ccx_1 = corners[index_id_1][0][1]
-    
-    ccx_2 = corners[index_id_2][0][0]
-    
-    ccx_3 = corners[index_id_3][0][2]
+# Инициализация времени
+start_time = time.time()
+def estimatePoseBoard(corners, ids, board, camera_matrix, dist_coeffs):
    
-    ccx_4 = corners[index_id_4][0][3]
-    
-    point = np.array([
-        [[ccx_2[0], ccx_2[1]],   
-        [ccx_1[0], ccx_1[1]], 
-        [ccx_3[0], ccx_3[1]], 
-        [ccx_4[0], ccx_4[1]]]
-    ], dtype=np.float32)
-    return point
+    if ids is None or len(ids) == 0:
+        return False, None, None
 
+    object_points = [] 
+    image_points = []
+    print(board.getIds())
+    print(ids)
+    # Итерируем по всем маркерам доски
+    for i, marker_id in enumerate(ids.flatten()):
+        if marker_id in ids:
 
-def my_estimatePoseSingleMarkers(corners, marker_size, mtx, distortion):
-    marker_points = np.array([[-marker_size / 2, marker_size / 2, 0],
-                              [marker_size / 2, marker_size / 2, 0],
-                              [marker_size / 2, -marker_size / 2, 0],
-                              [-marker_size / 2, -marker_size / 2, 0]], dtype=np.float32)
-    trash = []
-    rvecs = []
-    tvecs = []
-    for c in corners:
-        nada, R, t = cv2.solvePnP(marker_points, c, mtx, distortion, False, cv2.SOLVEPNP_IPPE_SQUARE)
-        rvecs.append(R)
-        tvecs.append(t)
-        trash.append(nada)
-    return rvecs, tvecs, trash
+            marker_idx = np.where(ids == marker_id)[0][0]
+  
+            obj_pts = board.getObjPoints()[marker_idx]
 
-# def my_estimatePoseSingleMarkers_3P(corners, central_point, marker_size, mtx, distortion):
-#     # Определение 3D координат трех известных точек маркера
-#     half_size = marker_size / 2
-#     marker_points_3d = np.array([
-#         [central_point[0] - half_size, central_point[1] + half_size, 0],  # Верхний левый угол
-#         [central_point[0] + half_size, central_point[1] - half_size, 0],  # Верхний правый угол
-#         [central_point[0] - half_size, central_point[1] - half_size, 0]
-#     ], dtype=np.float32)
+            img_pts = corners[i].reshape(-1, 2)
 
-    
-#     for c in corners:
-#         # Используем только три угла
-#         selected_corners = c[:3]
+            object_points.append(obj_pts)
+            image_points.append(img_pts)
 
-#         # Решение задачи PnP с тремя точками без начальных значений
-#         success, rvec, tvec = cv2.solvePnP(marker_points_3d, selected_corners, mtx, distortion, flags=cv2.SOLVEPNP_SQPNP)
-        
-#         if success:
-#             rvecs.append(rvec)
-#             tvecs.append(tvec)
-#         else:
-#             print("Ошибка при решении PnP")
+    if len(object_points) == 0:
+        return False, None, None
 
-#     return rvecs, tvecs
+    object_points = np.concatenate(object_points, axis=0)
+    image_points = np.concatenate(image_points, axis=0)
 
-def draw_axes_at_center_of_markers(frame, center_1, camera_matrix, dist_coeffs):
-    if len(corners) < 3:
-        print("Недостаточно маркеров для расчета центра.")
-        return
+    retval, rvec, tvec = cv2.solvePnP(
+        object_points, image_points, camera_matrix, dist_coeffs
+    )
+    return retval, rvec, tvec
 
-    # Получаем 2D координаты углов для трех маркеров
-    center = np.array([center_1[0], center_1[1], 0.0], dtype=np.float32).reshape((3,1))
-    axis_length = 0.1  # Длина осей
-    axes = np.array([[0.1, 0, 0],    # Ось X
-                     [0, 0.1, 0],    # Ось Y
-                     [0, 0, 0.1]],   # Ось Z
-                    dtype=np.float32)
-    axes += center.T
-    # Проецируем 3D оси на 2D изображение
-    image_points, _ = cv2.projectPoints(axes, np.zeros((3, 1), dtype=np.float32), center, camera_matrix, dist_coeffs)
+def board_corn_id(marker_length, offset):
+    board_corners = []
+    board_ids = []
 
-    # Рисуем оси
-    origin = tuple(image_points[0].flatten().astype(int))  # Начальная точка (центр)
-    x_axis = tuple(image_points[1].flatten().astype(int))  # Ось X
-    y_axis = tuple(image_points[2].flatten().astype(int))  # Ось Y
-    z_axis = tuple(image_points[3].flatten().astype(int))  # Ось Z, но игнорируется при отрисовке
+    side_ids = [
+        [0, 1, 2, 3],  # Сторона 1
+        [4, 5, 6, 7],  # Сторона 2
+        [8, 9, 10, 11],  # Сторона 3
+        [12, 13, 14, 15],  # Сторона 4
+        [16, 17, 18, 19],  # Сторона 5
+        [20, 21, 22, 23],  # Сторона 6
+    ]
 
-    # Рисуем оси на изображении
-    cv2.line(frame, origin, x_axis[:2], (255, 0, 0), 2)  # Ось X - красная
-    cv2.line(frame, origin, y_axis[:2], (0, 255, 0), 2)  # Ось Y - зеленая
-    cv2.line(frame, origin, z_axis[:2], (0, 0, 255), 2)  # Ось Z - синяя (если н
+    # Координаты для каждой стороны
+    side_offsets = [
+        [0, 0, -offset],  # Передняя сторона
+        [offset, 0, 0],  # Правая сторона
+        [0, 0, offset],  # Задняя сторона
+        [-offset, 0, 0],  # Левая сторона
+        [0, -offset, 0],  # Верхняя сторона
+        [0, offset, 0],  # Нижняя сторона
+    ]
 
+    for i, ids in enumerate(side_ids):
+        x_offset, y_offset, z_offset = side_offsets[i]
+        corners = [
+            [x_offset - marker_length / 2, y_offset - marker_length / 2, z_offset],
+            [x_offset + marker_length / 2, y_offset - marker_length / 2, z_offset],
+            [x_offset + marker_length / 2, y_offset + marker_length / 2, z_offset],
+            [x_offset - marker_length / 2, y_offset + marker_length / 2, z_offset],
+        ]
+        for j in range(4):  # Один маркер на каждой позиции
+            board_corners.append(np.array(corners))
+            board_ids.append(ids[j])
+
+    # Преобразуем данные в формат OpenCV
+    board_corners = np.array(board_corners, dtype=np.float32)
+    board_ids = np.array(board_ids, dtype=np.int32)
+    return board_corners, board_ids
+
+camera_matrix = np.array([
+                        [874.27826124, 0, 337.49038078],
+                        [0, 867.79031673, 196.680340],
+                        [0, 0, 1]], dtype=np.float32)
+
+dist_coeffs = np.array([0.57624005, -3.3464065, -0.01874216, 0.05337322, 2.87051004])
 
 aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)  # Подставь нужный словарь
 parameters = cv2.aruco.DetectorParameters()
 
+board_corners, ids = board_corn_id(marker_length, offset)
+board = cv2.aruco.Board(board_corners, aruco_dict, ids)
+detector = cv2.aruco.ArucoDetector(aruco_dict, parameters)
+
 cap = cv2.VideoCapture(0)
+plt.ion()  # Включаем интерактивный режим
+fig, axs = plt.subplots(3, 1, figsize=(10, 8))
+fig.suptitle('Real-Time Roll, Pitch, Yaw')
 
-camera_matrix = np.array([[4577.7689562929327, 0, 341.35776797777669],
-                          [0, 1845.0965778655157, 189.94836015559704],
-                          [0, 0, 1]], dtype=np.float32)
-dist_coeffs = np.array([27.505983603416613, -651.00215745718265, -1.1253795213096234,
-       -0.18015690540560628, 4713.9105972395546 ])
+axs[0].set_title('Roll')
+axs[0].set_ylim(-180, 180)
+axs[0].set_xlim(0, buffer_size)
+axs[0].grid(True)
+line_roll, = axs[0].plot([], [], 'r-')
 
-marker_size = 0.01 
-virtual_size = 0.02
-data = []
-data_y = []
-i = 0
+axs[1].set_title('Pitch')
+axs[1].set_ylim(-180, 180)
+axs[1].set_xlim(0, buffer_size)
+axs[1].grid(True)
+line_pitch, = axs[1].plot([], [], 'g-')
+
+axs[2].set_title('Yaw')
+axs[2].set_ylim(-180, 180)
+axs[2].set_xlim(0, buffer_size)
+axs[2].grid(True)
+line_yaw, = axs[2].plot([], [], 'b-')
+
+plt.tight_layout(rect=[0, 0.03, 1, 0.95])
 while True:
+    
     ret, frame = cap.read()
     if not ret:
         break
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    detector = cv2.aruco.ArucoDetector(aruco_dict, parameters)
-    (corners, ids, rejected) = detector.detectMarkers(gray)
-    cv2.aruco.drawDetectedMarkers(frame, corners, ids)
-    if ids is not None:
-        rvecs, tvecs, _ = my_estimatePoseSingleMarkers(corners, marker_size, camera_matrix, dist_coeffs)
-        indices_id_1 = np.where(ids == 1)[0]
-        indices_id_2 = np.where(ids == 0)[0]
-        indices_id_3 = np.where(ids == 5)[0]
-        indices_id_4 = np.where(ids == 4)[0]
-
-        if indices_id_1.size > 0 and indices_id_4.size > 0 and indices_id_3.size > 0 and indices_id_2.size > 0:
-
-            index_id_1 = indices_id_1[0]
-            index_id_2 = indices_id_2[0]
-            index_id_3 = indices_id_3[0]
-            index_id_4 = indices_id_4[0]
-            point = give_points(corners, index_id_1, index_id_2, index_id_3, index_id_4)
-            # x, y = funcs.find_center(corners)
-            # print(x, y)
-            # cv2.circle(frame, (int(x), int(y)), 5, (255,0,0), -1)
-            lenBott = math.sqrt((point[0][2][0] - point[0][3][0])**2 + (point[0][2][1] - point[0][3][1])**2) / 2
-            rvecs1, tvecs1, _ = my_estimatePoseSingleMarkers(point, virtual_size, camera_matrix, dist_coeffs)
-            cv2.drawFrameAxes(frame, camera_matrix, dist_coeffs, rvecs1[0], tvecs1[0], 0.01)
-            cv2.drawContours(frame, [point.astype(np.int32)], -1, (160, 120, 255), thickness=2)
-            
-
-            # front_points = np.array([
-            #     [point[0][0][0], point[0][0][1]],
-            #     [point[0][1][0], point[0][1][1]], 
-            #     [point[0][2][0], point[0][2][1]], 
-            #     [point[0][3][0], point[0][3][1]]
-            # ], dtype=np.int32)
-            # back_square = front_points - [0, int(lenBott)]
-            # draw_cube(frame, front_points, back_square)
-
-            # # aruco_angle(rvecs1, tvecs1)
-            # calc_angle_rotation(tvecs1, rvecs1) #та самая 
-            
-            # roll, pitch, yaw = calc_ang(tvecs1, rvecs1)
-            
-        elif indices_id_4.size > 0 and indices_id_3.size > 0 and indices_id_2.size > 0:
-            
-            index_id_2 = indices_id_2[0]
-            index_id_3 = indices_id_3[0]
-            index_id_4 = indices_id_4[0]
-
-            ccx_2 = corners[index_id_2][0][0].astype(int)
-            
-            ccx_3 = corners[index_id_3][0][2].astype(int)
+    corners, ids_mark, _ = detector.detectMarkers(gray)
+    
+    if ids_mark is not None:
+        retval, rvec, tvec = estimatePoseBoard(corners, ids_mark, board, camera_matrix, dist_coeffs)
+        cv2.drawFrameAxes(frame, camera_matrix, dist_coeffs, rvec, tvec, 0.01)
+        roll, pitch, yaw = calc_ang(tvec, rvec)
+        current_time = time.time() - start_time
+        time_data.append(current_time)
+        roll_data.append(roll)
+        pitch_data.append(pitch)
+        yaw_data.append(yaw)
         
-            ccx_4 = corners[index_id_4][0][3].astype(int)
+        # Обновляем графики
+        line_roll.set_xdata(range(len(roll_data)))
+        line_roll.set_ydata(roll_data)
+        axs[0].set_xlim(max(0, len(roll_data)-buffer_size), len(roll_data))
+        axs[0].relim()
+        axs[0].autoscale_view(True, True, True)
+        
+        line_pitch.set_xdata(range(len(pitch_data)))
+        line_pitch.set_ydata(pitch_data)
+        axs[1].set_xlim(max(0, len(pitch_data)-buffer_size), len(pitch_data))
+        axs[1].relim()
+        axs[1].autoscale_view(True, True, True)
+        
+        line_yaw.set_xdata(range(len(yaw_data)))
+        line_yaw.set_ydata(yaw_data)
+        axs[2].set_xlim(max(0, len(yaw_data)-buffer_size), len(yaw_data))
+        axs[2].relim()
+        axs[2].autoscale_view(True, True, True)
+        
+        # Перерисовка графиков
+        plt.pause(0.001)
+    cv2.imshow("Video", frame)
 
-            p1 = np.array([ccx_2[0], ccx_2[1]])  # Маркер 1
-            p2 = np.array([ccx_3[0], ccx_3[1]])  # Маркер 2
-            p3 = np.array([ccx_4[0], ccx_4[1]])  # Маркер 3
-
-            # Середина между p1 и p2
-            midpoint = (p1 + p2) / 2
-
-            # Предсказанная точка как зеркальное отражение p3 относительно midpoint
-            predicted_point = 2 * midpoint - p3
-            points = np.array([
-                [[ccx_2[0], ccx_2[1]],
-                [predicted_point[0].astype(int), predicted_point[1].astype(int)],   
-                [ccx_3[0], ccx_3[1]], 
-                [ccx_4[0], ccx_4[1]]]
-            ], dtype=np.float32)
-            print([predicted_point[0], predicted_point[1]])
-            rvec, tvec, _ = my_estimatePoseSingleMarkers(points, virtual_size, camera_matrix, dist_coeffs)
-            cv2.drawFrameAxes(frame, camera_matrix, dist_coeffs, rvec[0], tvec[0], 0.01)
-
-            # rvec, tvec = my_estimatePoseSingleMarkers_3P(point, [424.75, 362.0], 0.04, camera_matrix, dist_coeffs)
-            # draw_axes_at_center_of_markers(frame, [424.75, 362.0], camera_matrix, dist_coeffs)
-            # cv2.drawFrameAxes(frame, camera_matrix, dist_coeffs, rvec[0], tvec[0], 0.01)
-            cv2.drawContours(frame, [points.astype(np.int32)], -1, (160, 120, 255), thickness=2)
-
-    cv2.imshow('ArUco Marker Detection', frame)
-
+    # Выход по клавише ESC
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
 cv2.destroyAllWindows()
+plt.ioff()
+plt.show()
